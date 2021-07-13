2610fa1de57822346907a962109e6bdde92768c6
TeemoTrader <austinnorman1995@gmail.com>
2021-07-13 16:15:10 -0400

    Initial Commit

---
+++ H4XOR NEWS/H4XOR_NEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NEWSApp.swift
+//  H4XOR NEWS
+//
+//  Created by Teemo Norman on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
